448f0dc1b8b7c09913e2c720a2fc6528a6fecfed
Ed Carlos Rocha <edcarlos_rocha_11@hotmail.com>
2021-05-10 12:34:12 -0300

    Adicionado Observer para fazer autorizações de transferencias

---
+++ app/Observers/TransactionObserver.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Observers;
+use Http;
+use App\Transaction;
+
+class TransactionObserver
+{
+    /**
+     * Handle the transaction "created" event.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return void
+     */
+    public function created(Transaction $transaction)
+    {
+        $response = Http::get('https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6');
+
+        $transaction->authorized = $response->successful();
+        $transaction->authorization_http_status = $response->status();
+        $transaction->authorization_response = json_encode($response->json());
+        $transaction->save();
+    }
+
+    /**
+     * Handle the transaction "updated" event.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return void
+     */
+    public function updated(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the transaction "deleted" event.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return void
+     */
+    public function deleted(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the transaction "restored" event.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return void
+     */
+    public function restored(Transaction $transaction)
+    {
+        //
+    }
+
+    /**
+     * Handle the transaction "force deleted" event.
+     *
+     * @param  \App\Transaction  $transaction
+     * @return void
+     */
+    public function forceDeleted(Transaction $transaction)
+    {
+        //
+    }
+}
